91dd75d2f854b2d6eeaf4fb304178be0e890daeb
Mohan Raj <mohanraj1311@gmail.com>
2014-11-23 00:48:59 -0500

    pythonScriptCalling&ChnagePassword&viewCapacity

--- sendJson.py
+++ sendJson.py
@@ -2,7 +2,8 @@ import json
 import requests
 
 
-url = "http://localhost:7172/register"
-data = {'username':'raju84', 'password':'123'}
+#url = "http://localhost:9181/register"
+url = "http://localhost:9181/changePassword"
+data = {'username':'raju84', 'oldPassword':'4567','newPassword':'567'}
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 r = requests.post(url, data=json.dumps(data), headers=headers)
--- server.js
+++ server.js
@@ -9,9 +9,14 @@ var morgan = require('morgan');        // logging
 var mongoose   = require('mongoose');  // DB connection
 
 var app = express();  // creating the express instance.
-var port = 7172;  // If you change this , then change the port in sendJson.py as well.
+var port = 8190;  // If you change this , then change the port in sendJson.py as well.
 
+//----------------BEGIN: PATH FOR ALL FUNCTIONS FOR DIFFERENT ROUTES SUPPORTED---------
 var register = require('./config/register');
+var changePassword = require('./config/changePassword');
+var viewCapacity = require('./config/viewCapacity');
+var viewSensorCapacity=require('./config/viewSensorCapacity');
+//----------------BEGIN: PATH FOR ALL FUNCTIONS FOR DIFFERENT ROUTES SUPPORTED---------
 
 // using the modules imported above.
 app.use(express.static(__dirname + '/public'));
@@ -21,14 +26,74 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 //Cloud DB connection - log in to mongolab.com to see.
-var username = "mobdb"
-var password = "mobdb";
-var address = '@ds043220.mongolab.com:43220/mobile';
+var username = "mobile"
+var password = "mobile123";
+var address = '@ds047940.mongolab.com:47940/mobile';
 var url = 'mongodb://' + username + ':' + password + address;
 
 
 //Connect DB
 mongoose.connect(url);
+
+//--------------------BEGIN: CODE FOR CALLING THE ROUTER-PYTHON SCRIPT REGULARLY ---------------------
+
+var fs = require("fs");
+var outputFilepath = "./sample.txt";
+var routTable = require('./app/models/routTable');
+
+var pythonScriptCallingFunction = function(){
+
+var PythonShell = require('python-shell');
+var scriptpath = "../sendJson.py";
+
+PythonShell.run(scriptpath, function (err) {
+  if (err) 
+  	console.log(err);
+  
+});
+
+
+
+require('readline').createInterface({
+    input: fs.createReadStream(outputFilepath),
+    terminal: false
+}).on('line', function(line){
+   //console.log('Line: ' + line);
+   	var dataArr = line.split(' ');
+   
+	var newRouterData = new routTable();
+	newRouterData.ip = dataArr[0];
+	newRouterData.mac = dataArr[1];
+	newRouterData.device_name = dataArr[2];
+	newRouterData.floor = 4;
+	newRouterData.lib_name = "Marston";
+	newRouterData.saved_on = new Date();
+
+
+	var conditions = { mac: dataArr[1] };
+	var update = { $set: { saved_on: new Date(), ip:dataArr[0],floor : 4, lib_name:"Marston",device_name:dataArr[2]}};
+	var options = { upsert: true };
+	var mycallback = function(err,result){
+		if(err)
+			console.log(err);
+	}
+
+	routTable.update(conditions, update, options, mycallback);
+
+	
+
+});//End of the linewise dealing function
+
+
+}// End of function : pythonSciptCalling function
+
+var setDelay = 3*1000; // This is the delay time for each call. Its in milli seconds.
+
+setInterval(pythonScriptCallingFunction, setDelay);
+
+
+//--------------------END: CODE FOR CALLING THE ROUTER-PYTHON SCRIPT REGULARLY ---------------------
+
 
 
 
@@ -62,8 +127,50 @@ router.route('/register')
 	});
 });
 
+//Begin : changePassword
+router.route('/changePassword')
+	.post(function(req, res){
+		var username = req.body.username;
+		var oldPassword = req.body.oldPassword;
+		var newPassword = req.body.newPassword;
+		changePassword.changePassword(username, oldPassword, newPassword, function(result){
+			//console.log(result);
+			res.json(result);
+		});
 
-// ---------------------------ALL OUR ROUTES SHALL BE HERE -------------------------------
+	});	
+
+//---------------------------BEGIN : ROUTES BY RASHMI----------------------------------------------
+router.route('/viewCapacity')
+router.get('/viewCapacity', function(req, res) {
+	var libraryList = {
+      "libraries" : ['Marston', 'Smathers', 'West']
+    };
+    res.json(libraryList);   
+});
+
+var lib_capacity_marston=120;
+router.route('/viewCapacity/*')
+router.get('/viewCapacity/*', function(req, res) {
+	   var lib_name=req.url;
+	   lib_name=lib_name.slice(14);
+
+	  viewCapacity.viewCapacity(lib_name,function (routerCount) {
+	  	console.log(routerCount,"routerCount")
+	        viewSensorCapacity.viewSensorCapacity(lib_name,function (sensorCount){
+			console.log(sensorCount,"sensorCount")
+			var cap=Math.round((routerCount+sensorCount)*100/(2*lib_capacity_marston));
+			cap=cap+"%";
+			res.json({"lib_capacity":cap});
+			});
+      });
+	   
+});
+
+//---------------------------END: ROUTES BY RASHMI----------------------------------------------
+
+
+// ---------------------------ALL OUR ROUTES END HERE--------------------------------------------
 
 
 // all of our routes will be prefixed with /
